0582a8675fe8f487ed15cfd225369df6a242179f
Chandana Mutgi <chmutgi@gmail.com>
2016-10-17 02:10:32 -0700

    updated start logstash

--- start_logstash.sh
+++ start_logstash.sh
@@ -1,1 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
+/opt/logstash/bin/logstash  -f replaceme_with_logstashconf >> /var/log/asup_client.logs &
+echo "logstash started; logs redirected to /var/log/asup_client.logs"
